39b912e681f95619ce4a557f94c4d2b1eb57927c
LillianWight <lillian.wight@gmail.com>
2022-04-18 15:24:22 -0600

    End of Lesson 84 Switching the Active Player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.getElementById('current--0');
@@ -16,8 +18,9 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 //rolling dice functionality
-
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -26,6 +29,13 @@ btnRoll.addEventListener('click', function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    currentScore = 0;
   }
 });
