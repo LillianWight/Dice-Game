b9328f3dba533c80bef90e6773024fbf3e9e22ec
LillianWight <lillian.wight@gmail.com>
2022-04-18 14:58:32 -0600

    End of Lesson 83 Rolling the Dice

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 'use strict';
 
 //selecting elements
-const score0Element = document.querySelector('#score--0');
-const score1Element = document.querySelector('#score--1');
-const diceElement = document.querySelector('.dice');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //starting conditions
-score0Element.textContent = 0;
-score1Element.textContent = 0;
-diceElement.classList.add('hidden');
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//rolling dice functionality
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  }
+});
